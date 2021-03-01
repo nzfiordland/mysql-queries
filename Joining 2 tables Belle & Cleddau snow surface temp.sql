4e4191913b7d565cca19ea49d5ffe910188c540e
Dylan O'Neill <dylan.oneill@downer.co.nz>
2021-03-02 10:42:31 +1300

    Update Joining 2 tables Belle & Cleddau snow surface temp.sql

--- Joining 2 tables Belle & Cleddau snow surface temp.sql
+++ Joining 2 tables Belle & Cleddau snow surface temp.sql
@@ -1,9 +1,9 @@
 SELECT 
-    nzta_belle1_hour.TmStamp,
+    nzta_belle1_aws_10.TmStamp,
     nzta_belle1_aws_10.IR_SnowSurfTemp AS 'Belle snow surface temp',
     nzta_cleddau1_aws_10.IR_SnowSurfTemp AS 'Cleddau snow surface temp'
 FROM
     nzta_belle1_aws_10,
     nzta_cleddau1_aws_10
 WHERE 
-    nzta_belle1_aws_10.TmStamp >= '2020-11-20 00:00:00'
+    nzta_belle1_aws_10.TmStamp >= '2021-03-02 00:00:00'
